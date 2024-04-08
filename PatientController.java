76514707ffac07ee56c940aa0046e91e1a8d85a0
SinghOPS <contact.sahajpreetsingh@gmail.com>
2024-04-07 23:33:48 -0700

    Initial commit for patient view classes and UI

---
+++ PatientController.java
@@ -0,0 +1,92 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+
+public class PatientController {
+
+    @FXML
+    private TableView<?> appointmentsTable;
+
+    @FXML
+    private Button bookAppointmentButton;
+
+    @FXML
+    private Button healthRecordButton;
+
+    @FXML
+    private TableView<?> immunizationsTable;
+
+    @FXML
+    private Button insuranceButton;
+
+    @FXML
+    private Button prescriptionsButton;
+
+    @FXML
+    void appointmentsTabView(ActionEvent event) {
+
+    }
+
+    @FXML
+    void bookAppBtnClick(ActionEvent event) {
+        /*try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlFileName));
+            Parent page = loader.load();
+            Scene scene = new Scene(page);
+            Stage stage = (Stage) landingPage.getScene().getWindow();
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+    }
+
+    @FXML
+    void healthRecBtnClick(ActionEvent event) {
+        /*try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlFileName));
+            Parent page = loader.load();
+            Scene scene = new Scene(page);
+            Stage stage = (Stage) landingPage.getScene().getWindow();
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+    }
+
+    @FXML
+    void immunizationsTabView(ActionEvent event) {
+
+    }
+
+    @FXML
+    void insuranceBtnClick(ActionEvent event) {
+        /*try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlFileName));
+            Parent page = loader.load();
+            Scene scene = new Scene(page);
+            Stage stage = (Stage) landingPage.getScene().getWindow();
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+    }
+
+    @FXML
+    void prescriptionsBtnClick(ActionEvent event) {
+        /*try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlFileName));
+            Parent page = loader.load();
+            Scene scene = new Scene(page);
+            Stage stage = (Stage) landingPage.getScene().getWindow();
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+    }
+
+}
---
+++ ViewController.java
@@ -0,0 +1,94 @@
+//package application;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class ViewController {
+	private Stage primaryStage;
+
+    @FXML
+    private Button doctorViewButton;
+
+    @FXML
+    private Button loginViewButton;
+
+    @FXML
+    private Button nurseViewButton;
+
+    @FXML
+    private Button patientViewButton;
+
+  
+    @FXML
+    void doctorView(ActionEvent event) {
+    	/*
+    	try {
+    		Parent root = FXMLLoader.load(getClass().getResource("DoctorView.fxml"));
+    		Scene scene = new Scene(root);
+		
+    		primaryStage.setTitle("Nurse View");
+    		primaryStage.setScene(scene);
+    		primaryStage.show();
+    	} catch (IOException e) {
+    		e.printStackTrace();
+    	}
+    	*/
+    }
+
+    @FXML
+    void loginView(ActionEvent event) {
+    	/*
+    	try {
+    		Parent root = FXMLLoader.load(getClass().getResource("LoginView.fxml"));
+    		Scene scene = new Scene(root);
+		
+    		primaryStage.setTitle("Nurse View");
+    		primaryStage.setScene(scene);
+    		primaryStage.show();
+    	} catch (IOException e) {
+    		e.printStackTrace();
+    	}
+    	*/
+    }
+
+    @FXML
+    void nurseView(ActionEvent event) {
+    	/*try {
+    		Parent root = FXMLLoader.load(getClass().getResource("NurseView.fxml"));
+    		Scene scene = new Scene(root);
+		
+    		primaryStage.setTitle("Nurse View");
+    		primaryStage.setScene(scene);
+    		primaryStage.show();
+    	} catch (IOException e) {
+    		e.printStackTrace();
+    	}
+        */
+    }
+
+    @FXML
+    void patientView(ActionEvent event) {
+    	try {
+    		Parent root = FXMLLoader.load(getClass().getResource("PatientView.fxml"));
+    		Scene scene = new Scene(root);
+		
+    		primaryStage.setTitle("Nurse View");
+    		primaryStage.setScene(scene);
+    		primaryStage.show();
+    	} catch (IOException e) {
+    		e.printStackTrace();
+    	}
+    }
+    
+    public void setStage(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+    }
+
+}
