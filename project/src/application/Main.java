a92849d3e1b5a3436e2c06f5801b25cf2a5d9568
dyellow3 <dyellow3@asu.edu>
2024-04-10 20:26:43 -0700

    final

--- project/src/application/Main.java
+++ project/src/application/Main.java
@@ -8,11 +8,6 @@ import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 
-// Main is our system?
-// if it is it should create the users (nurse, doctor, patient) and store in database
-// Main should be the controller, responsible for showing views
-// it should start by displaying login view
-
 public class Main extends Application {
 	private static final String FOLDER_NAME = "user_info";
 	@Override
